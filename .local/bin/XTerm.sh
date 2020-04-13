a493db44fe8c0fee7e2b9845efbb7101015b08ff
Pavel SHumskii <shumskii@gmail.com>
2020-04-14 05:44:52 +0700

    add screen-workspace 3rd level screen

--- .local/bin/XTerm.sh
+++ .local/bin/XTerm.sh
@@ -8,10 +8,7 @@ cd "${HOME}"
 
 # Exec xterm, use GNU Screen instead of user's shell
 exec xterm\
-     -e screen\
-        -c ~/.screenrc-tabber\
-        -s 'menu'\
-        -S "${HOSTNAME}-tabber"\
-        -qDRO
+     -e "${HOME}/.screenrc-workspace"\
+        -S "${HOSTNAME}-workspace"
 
 exit 1
